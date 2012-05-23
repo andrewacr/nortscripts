96210831f7515be04a3e6c718d3b114af69eea1e
Andrew Brockert <andrew@acresearchinc.com>
2012-05-23 22:31:02 +0000

    code

---
+++ examples/portlist.cpp
@@ -0,0 +1,18 @@
+#include <dashel/dashel.h>
+#include <iostream>
+
+using namespace Dashel;
+
+int main()
+{
+	std::map<int, std::pair<std::string, std::string> > ports = SerialPortEnumerator::getPorts();
+
+	for(std::map<int, std::pair<std::string, std::string> >::iterator it = ports.begin(); it != ports.end(); ++it)
+	{
+		std::cout << "(" << it->first << ") " << it->second.first << " [" << it->second.second << "]" << std::endl;
+	}
+
+	//system("pause");
+
+	return 0;
+}
